decafa58ef6da68b75e972a788d1fe1e655a2297
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-09-30 14:19:45 +0100

    feat: add helpers file

---
+++ src/screens/Welcome/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+// import styles from "./style";
+
+const Welcome = () => {
+  return (
+    <View>
+      <Text>"Welcome to Customer Manager Plus!"</Text>
+    </View>
+  );
+};
+
+export default Welcome;
---
+++ src/utils/helpers.js
@@ -0,0 +1,7 @@
+export const regions = [
+  "North West",
+  "Mid West",
+  "South West",
+  "South East",
+  "North East",
+];
