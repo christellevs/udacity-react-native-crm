d87873a992f7cc27442de57b824d776d20945ff0
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-10-02 12:58:35 +0100

    feat: add form

---
+++ src/features/customer/Form/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { useUpdateFields, useNewCustomer } from "..//hooks";
+import formStyles from "./styles";
+
+const Form = ({ disabled = false }) => {
+  const styles = StyleSheet.create(formStyles());
+  const { fields, setFormField } = useUpdateFields();
+  const { onSubmit } = useNewCustomer();
+  const [value, onChangeText] = React.useState("");
+
+  console.log();
+
+  console.log(setFormField);
+  // const {
+  //     common_name,
+  //     scientific_name,
+  //     description,
+  //     endangered_status,
+  //     population,
+  //     invasive
+  // } = fields
+
+  const { firstName, lastName } = fields;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.form}></View>
+
+      <TextInput
+        key={"firstName"}
+        placeholder="First Name"
+        value={firstName || ""}
+        style={{
+          borderWidth: 1,
+          borderColor: "black",
+          borderRadius: 4,
+          padding: 15,
+        }}
+        onChangeText={(v) => setFormField("firstName", v)}
+      />
+
+      <View style={{ height: 15, width: "100%" }}></View>
+
+      <TextInput
+        key={"lastName"}
+        placeholder="Last Name"
+        value={lastName || ""}
+        style={{
+          borderWidth: 1,
+          borderColor: "black",
+          borderRadius: 4,
+          padding: 15,
+        }}
+        onChangeText={(v) => setFormField("lastName", v)}
+      />
+
+      <TouchableOpacity onPress={onSubmit}>
+        <Text>Submit</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Form;
--- src/screens/NewCustomer/index.js
+++ src/screens/NewCustomer/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Button from "../../components/Button";
 import styles from "./styles";
+import Form from "../../features/customer/Form";
 
 const NewCustomer = () => {
   const style = StyleSheet.create(styles());
   return (
     <View style={style.view}>
       <Text style={style.title}>New Customer Form</Text>
+      <Form />
     </View>
   );
 };
