d4695aa28eedb755d1753eb4f1096c93c7c2d95f
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-10-02 13:10:41 +0100

    feat: add index and load sagas

---
+++ src/features/customer/sagas/index.js
@@ -0,0 +1,7 @@
+import { all } from "redux-saga/effects";
+import { watchLoadCustomers } from "./load";
+import { watchCreateCustomer } from "./create";
+
+export default function* customer() {
+  yield all([watchLoadCustomers(), watchCreateCustomers()]);
+}
