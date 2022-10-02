519e2147ce7125b5fab1063d73fb4663cb294890
Christelle Van Sebroeck <christelle.marie@gmail.com>
2022-10-02 14:26:17 +0100

    style: improve style of new customer form

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -5,9 +5,6 @@ import styles from "./styles";
 const Button = ({ text, onPress }) => {
   const style = StyleSheet.create(styles());
 
-  // const onPress = () => {
-  //   console.log("continued pressed");
-  // };
   return (
     <TouchableOpacity style={style.button} onPress={onPress}>
       <Text style={style.text}>{text}</Text>
--- src/features/customer/Form/index.js
+++ src/features/customer/Form/index.js
@@ -1,16 +1,10 @@
 import React from "react";
-import {
-  StyleSheet,
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
+import { StyleSheet, View, TextInput } from "react-native";
 import { useUpdateFields, useNewCustomer } from "..//hooks";
-// import formStyles from "./styles";
+import Button from "../../../components/Button";
+import style from "./styles";
 
 const Form = ({ disabled = false }) => {
-  // const styles = StyleSheet.create(formStyles());
   const { fields, setFormField } = useUpdateFields();
   const { onSubmit } = useNewCustomer();
   const [value, onChangeText] = React.useState("");
@@ -31,39 +25,22 @@ const Form = ({ disabled = false }) => {
 
   return (
     <View>
-      <View></View>
-
       <TextInput
         key={"firstName"}
-        placeholder="First Name"
+        placeholder="First Name..."
         value={firstName || ""}
-        style={{
-          borderWidth: 1,
-          borderColor: "black",
-          borderRadius: 4,
-          padding: 15,
-        }}
+        style={style.textInput}
         onChangeText={(v) => setFormField("firstName", v)}
       />
-
-      <View style={{ height: 15, width: "100%" }}></View>
-
       <TextInput
         key={"lastName"}
-        placeholder="Last Name"
+        placeholder="Last Name..."
         value={lastName || ""}
-        style={{
-          borderWidth: 1,
-          borderColor: "black",
-          borderRadius: 4,
-          padding: 15,
-        }}
+        style={style.textInput}
         onChangeText={(v) => setFormField("lastName", v)}
       />
 
-      <TouchableOpacity onPress={onSubmit}>
-        <Text>Submit</Text>
-      </TouchableOpacity>
+      <Button onPress={onSubmit} text="Submit" />
     </View>
   );
 };
